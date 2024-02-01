b749424a2906a532acd6125e3a74a417e83f524e
Bendjy <bsaintpaul@bostonk12.org>
2024-02-01 10:37:54 -0500

    integer_input

---
+++ Python/integer_input2.py
@@ -0,0 +1,4 @@
+age = int(input("Please enter your age:"))
+print("You are",age,"years old!")
+years = int(input("Please enter an integer:"))
+print("In",years,"year you will be ",age + years,"years old!")
