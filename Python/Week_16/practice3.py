12a6f11d43016c67533fee53da27f7fdd6fb2d28
Bendjy <bsaintpaul@bostonk12.org>
2024-04-25 10:34:55 -0400

    Python

---
+++ Python/Week_16/practice3.py
@@ -0,0 +1,17 @@
+# Bendjy St - Paul
+# 2024-04-25
+
+# This is how you create an empty list
+a = []
+
+# This is how you create a list with values
+b = [1, 2, 3]
+
+print(a)
+print(b)
+
+a+=['bendjy']
+b+=[4]
+
+print(a)
+print(b)
