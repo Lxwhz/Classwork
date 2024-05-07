3e0280d390255f92b0c7b408bc5c51ae94350cab
Bendjy <bsaintpaul@bostonk12.org>
2024-05-07 10:21:37 -0400

    Python

---
+++ Python/Week_17/2024-05-07/check.py
@@ -0,0 +1,30 @@
+# Bendjy St - Paul
+# 2024-05-07
+
+# creating an empty list
+a = []
+
+# using the operator to add to the list
+a += [1]
+a += [2]
+a += [3]
+
+# printing the list
+print(a)
+
+# creating an empty list named b
+b = []
+
+# using the function to add to the list b
+b.append(1)
+b.append(2)
+b.append(3)
+
+#printing the list b
+print(b)
+
+# positional comparaison
+print(a==b)
+
+# object comparaison
+print(a is b)
