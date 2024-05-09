354e14c7d2a5e5769586f24200bf8960b300db64
Bendjy <bsaintpaul@bostonk12.org>
2024-05-09 08:00:47 -0400

    Python

---
+++ Python/Week_17/2024-05-08/lists.py
@@ -0,0 +1,12 @@
+# Bendjy St - Paul
+# 2024-05-08
+
+# creating an empty list
+lst = []
+
+# using a for loop to add numbers 1-100 to the list
+for b in range(1,101):
+    lst.append(b)
+
+# printing the list lst
+print(lst)
