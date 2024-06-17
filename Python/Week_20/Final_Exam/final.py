b0525acbd88f74e26980db4ab9e68ff3cd1cf034
Bendjy <bsaintpaul@bostonk12.org>
2024-06-17 10:16:02 -0400

    Python

---
+++ Python/Week_20/Final_Exam/final.py
@@ -0,0 +1,12 @@
+def main():
+    words = []
+    with open ('dictionary.txt', 'r') as f:
+        for line in f:
+            words.append(line.strip())
+    words.sort()
+    print('ABACK' in words)
+    print(words)
+
+
+if __name__ == '__main__':
+    main()
