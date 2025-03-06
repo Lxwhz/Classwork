9872dba877aeae00ead8c6fd7a23fe1d4ab65c07
430878 <bsaintpaul@bostonk12.org>
2025-03-06 14:13:33 -0500

    paint2

--- 2024-2025/python/Week_12/2025-03-03/plant2.py
+++ 2024-2025/python/Week_12/2025-03-03/plant2.py
@@ -1,8 +1,8 @@
 def main():
-    X = int(input("Pick a number: "))
-    Y = int(input("Pick another number: "))
+    X1 = int(input("Pick a number: "))
+    Y1 = int(input("Pick another number: "))
 
-    print(f"{X} + {Y} is {X * Y}")
+    print(f"{X1} + {Y1} is {X1 * Y1}")
 
 if __name__ == '__main__':
     main()
