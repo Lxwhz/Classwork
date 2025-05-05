1af0c318dc891ca75ca8003b2394e2656b7709b9
430878 <bsaintpaul@bostonk12.org>
2025-05-05 09:26:30 -0400

    Vocab

--- Python/PythonExercise.py
+++
@@ -1,9 +0,0 @@
-number = input("Please enter a number between 1 and 10.")
-
-numbers = int(number)
-
- if 1 <= numbers <= 10:
-    print(f"your number is:")
-
-else:
-    print("your number must be between 1 and 10.")
